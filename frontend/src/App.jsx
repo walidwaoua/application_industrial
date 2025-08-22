bd3c1d9a0b9649dbf9021497b244c988d221e63f
walid waoua <Walid.Waoua@emsi-edu.ma>
2025-08-22 12:53:21 +0100

    fix: add frontend as normal folder

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import { Routes,Route } from 'react-router-dom'; 
+ // Updated path to new Home.jsx
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+       
+      </Routes>
+    </div>
+  );
+}
+
+ 
+          
+export default App;
